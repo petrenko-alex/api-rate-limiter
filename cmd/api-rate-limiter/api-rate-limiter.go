4beb93c221f833b7d567d3547355ab3ba49eaa38
Alexander Petrenko <gafk555@gmail.com>
2023-10-30 13:34:36 +0300

    realize grpc server starting

--- cmd/api-rate-limiter/api-rate-limiter.go
+++ cmd/api-rate-limiter/api-rate-limiter.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"api-rate-limiter/internal/server"
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello from api-rate-limiter")
+	ctx, cancel := signal.NotifyContext(
+		context.Background(),
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGHUP,
+	)
+	defer cancel()
+
+	logg := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	srv := server.NewServer(
+		server.Options{
+			Host:           "localhost",
+			Port:           "4242",
+			ConnectTimeout: time.Second * 1,
+		},
+		logg,
+	)
+
+	logg.Info("Starting GRPC server...")
+
+	go func() {
+		<-ctx.Done() // todo: not work?
+
+		logg.Info("Stopping GRPC server...")
+		if err := srv.Stop(ctx); err != nil {
+			logg.Error("Failed to stop GRPC server: " + err.Error())
+		}
+
+		logg.Info("Server stopped.")
+	}()
+
+	err := srv.Start(ctx)
+	if err != nil {
+		logg.Error("Failed to start GRPC server: " + err.Error())
+		cancel()
+	}
 }
