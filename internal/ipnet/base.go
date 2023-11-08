06b1fd1bbeebabb43254566ffb170fb9973ed168
Alexander Petrenko <gafk555@gmail.com>
2023-11-08 21:07:03 +0300

    realize rule service

--- internal/ipnet/base.go
+++ internal/ipnet/base.go
@@ -6,4 +6,16 @@ type IRuleStorage interface {
 
 	GetForIP(ip string) (*Rules, error)
 	GetForType(ruleType RuleType) (*Rules, error)
+	Find(ip string, ruleType RuleType) (*Rules, error)
+}
+
+type IRuleService interface {
+	InWhiteList(ip string) (bool, error)
+	InBlackList(ip string) (bool, error)
+
+	WhiteListAdd(ip string) error
+	WhiteListDelete(ip string) error
+
+	BlackListAdd(ip string) error
+	BlackListDelete(ip string) error
 }
