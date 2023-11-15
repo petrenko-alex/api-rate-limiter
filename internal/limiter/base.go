a3470be4688736cb1403ffe962781c13877aacc9
Alexander Petrenko <gafk555@gmail.com>
2023-11-15 22:02:07 +0300

    add doc

--- internal/limiter/base.go
+++ internal/limiter/base.go
@@ -5,12 +5,16 @@ import "errors"
 // ErrIncorrectIdentity Ошибка на случай некорректного входного аргумента identity
 var ErrIncorrectIdentity = errors.New("not found appropriate key in user identity")
 
+// UserIdentityDto тип для идентификации клиента, запрос которого подвергается rate limit'ингу.
+// Может содержать один или несколько пар ключ-значение. Лимитеры сами решают, с какими ключами работать.
 type UserIdentityDto map[string]string
 
+// ILimitStorage хранилище лимитов (правил) rate limit'инга запросов.
 type ILimitStorage interface {
 	GetLimits() (*Limits, error)
 }
 
+// ILimitService основной сервис проверки запроса на rate limit.
 type ILimitService interface {
 	SatisfyLimit(UserIdentityDto) (bool, error)
 }
