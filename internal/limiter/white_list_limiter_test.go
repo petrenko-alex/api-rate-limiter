c3b9adfac91ef281b99d7298fb4440662a6b0148
Alexander Petrenko <gafk555@gmail.com>
2023-11-15 20:57:11 +0300

    realize WhiteListLimiter tests

--- internal/limiter/white_list_limiter_test.go
+++ internal/limiter/white_list_limiter_test.go
@@ -4,44 +4,74 @@ import (
 	"testing"
 
 	"github.com/petrenko-alex/api-rate-limiter/internal/ipnet"
+	ipnetmocks "github.com/petrenko-alex/api-rate-limiter/internal/ipnet/mocks"
 	"github.com/petrenko-alex/api-rate-limiter/internal/limiter"
 	"github.com/stretchr/testify/require"
 )
 
-// todo: use table test
 func TestWhiteListLimiter_SatisfyLimit(t *testing.T) {
-	strg := ipnet.NewRuleStorage("dsn") // todo: mock
-	srvc := ipnet.NewRuleService(strg)
+	rules := ipnet.Rules{
+		ipnet.Rule{
+			ID:       1,
+			IP:       "192.168.1.1",
+			RuleType: ipnet.WhiteList,
+		},
+		ipnet.Rule{
+			ID:       2,
+			IP:       "125.130.2.3",
+			RuleType: ipnet.WhiteList,
+		},
+		ipnet.Rule{
+			ID:       3,
+			IP:       "192.168.3.0/24",
+			RuleType: ipnet.WhiteList,
+		},
+	}
+	mockStorage := ipnetmocks.NewMockIRuleStorage(t)
+	mockStorage.EXPECT().GetForType(ipnet.WhiteList).Return(&rules, nil)
 
-	whiteListLimiter := limiter.NewWhiteListLimiter(srvc)
+	whiteListLimiter := limiter.NewWhiteListLimiter(
+		ipnet.NewRuleService(mockStorage),
+	)
 
-	t.Run("found by exact ip match", func(t *testing.T) {
-		satisfies, err := whiteListLimiter.SatisfyLimit(limiter.UserIdentityDto{"ip": "192.168.1.1"})
+	tests := []struct {
+		name     string
+		ip       string
+		expected bool
+	}{
+		{
+			name:     "found by exact ip match",
+			ip:       "192.168.1.1",
+			expected: true,
+		},
+		{
+			name:     "found by subnet match",
+			ip:       "192.168.3.25",
+			expected: true,
+		},
+		{
+			name:     "not found",
+			ip:       "222.113.10.3",
+			expected: false,
+		},
+	}
 
-		require.NoError(t, err)
-		require.True(t, satisfies)
-	})
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			satisfies, err := whiteListLimiter.SatisfyLimit(limiter.UserIdentityDto{"ip": test.ip})
 
-	t.Run("found by subnet match", func(t *testing.T) {
-		satisfies, err := whiteListLimiter.SatisfyLimit(limiter.UserIdentityDto{"ip": "125.110.13.25"})
-
-		require.NoError(t, err)
-		require.True(t, satisfies)
-	})
-
-	t.Run("not found", func(t *testing.T) {
-		satisfies, err := whiteListLimiter.SatisfyLimit(limiter.UserIdentityDto{"ip": "222.113.10.3"})
-
-		require.NoError(t, err)
-		require.False(t, satisfies)
-	})
+			require.NoError(t, err)
+			require.Equal(t, test.expected, satisfies)
+		})
+	}
 }
 
 func TestWhiteListLimiter_SatisfyLimit_Error(t *testing.T) {
 	t.Run("incorrect identity error", func(t *testing.T) {
-		strg := ipnet.NewRuleStorage("dsn") // todo: mock
-		srvc := ipnet.NewRuleService(strg)
-		whiteListLimiter := limiter.NewWhiteListLimiter(srvc)
+		mockStorage := ipnetmocks.NewMockIRuleStorage(t)
+		whiteListLimiter := limiter.NewWhiteListLimiter(
+			ipnet.NewRuleService(mockStorage),
+		)
 
 		identity := limiter.UserIdentityDto{"login": "admin"} // white list limiter needs ip
 		_, err := whiteListLimiter.SatisfyLimit(identity)
