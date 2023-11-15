f3e5c1bcfe64fbf06bec28e6f90faecbb10b45eb
Alexander Petrenko <gafk555@gmail.com>
2023-11-15 21:56:51 +0300

    realize BlackListLimiter and tests

--- internal/limiter/black_list_limiter.go
+++ internal/limiter/black_list_limiter.go
@@ -1,16 +1,31 @@
 package limiter
 
+import "github.com/petrenko-alex/api-rate-limiter/internal/ipnet"
+
+const BlackListLimiterIdentityKey = "ip"
+
 // BlackListLimiter лимитер на основе принципа черного списка.
 // Если клиент есть в черном списке, то считается, что он всегда не попадает в лимит.
 type BlackListLimiter struct {
+	ruleService ipnet.IRuleService
+}
+
+func NewBlackListLimiter(service *ipnet.RuleService) BlackListLimiter {
+	return BlackListLimiter{
+		ruleService: service,
+	}
 }
 
 func (l BlackListLimiter) SatisfyLimit(identity UserIdentityDto) (bool, error) {
-	// Get ip from identity
-	// 		error if no ip
-	// Use RuleService::InBlackList() to check.
-	// If TRUE - return FALSE
-	// IF FALSE - return TRUE
+	ip, found := identity[BlackListLimiterIdentityKey]
+	if !found {
+		return false, ErrIncorrectIdentity
+	}
 
-	panic("implement me")
+	inBlackList, err := l.ruleService.InBlackList(ip)
+	if err != nil {
+		return false, err
+	}
+
+	return !inBlackList, nil
 }
