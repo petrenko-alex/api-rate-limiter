73bd7b60784acaf821e373902b1325098a71c8ea
alex <gafk555@gmail.com>
2023-11-22 23:43:57 +0300

    add mutex for TokenBucket

--- internal/limiter/token_bucket.go
+++ internal/limiter/token_bucket.go
@@ -1,11 +1,13 @@
 package limiter
 
 import (
+	"sync"
 	"time"
 )
 
-// todo: thread safe
 type TokenBucket struct {
+	sync.RWMutex
+
 	size       int
 	refillRate RefillRate
 
@@ -36,6 +38,8 @@ func NewRefillRate(count int, time time.Duration) RefillRate {
 }
 
 func (b *TokenBucket) Refill() {
+	b.Lock()
+
 	const nsInSec = 1e9
 	timePassed := time.Since(b.lastRefill)
 	tokensToAdd := int64(timePassed) * int64(b.refillRate.count) / int64(nsInSec*b.refillRate.time.Seconds())
@@ -44,19 +48,32 @@ func (b *TokenBucket) Refill() {
 	if tokensToAdd > 0 {
 		b.lastRefill = time.Now()
 	}
+
+	b.Unlock()
 }
 
 func (b *TokenBucket) GetTokenCount() int {
+	b.RLock()
+	defer b.RUnlock()
+
 	return b.tokensCount
 }
 
 func (b *TokenBucket) GetToken(tokenCount int) {
+	b.Lock()
+
 	b.tokensCount -= tokenCount
+
+	b.Unlock()
 }
 
 func (b *TokenBucket) Reset() {
+	b.Lock()
+
 	b.tokensCount = b.size
 	b.lastRefill = time.Now()
+
+	b.Unlock()
 }
 
 func (r RefillRate) GetTime() time.Duration {
